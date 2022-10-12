f1edc676e1454ce21d24896f3dbfb2ede92925f4
kota satish <113907436+satipotti@users.noreply.github.com>
2022-10-12 16:34:56 +0530

    n=int(input()) if n%9==0:     print(9) else:     print(n%9)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,14 +1,5 @@
-n = int(input())
-s = 0
-while n > 0:
-    r = n % 10
-    s += r
-    n = n // 10
-    if n==0 and s>9:
-        n=s
-        s=0
-print(s)    
-        
-
-
-    
+n=int(input())
+if n%9==0:
+    print(9)
+else:
+    print(n%9)
